Rockbox-Chinese-Community/Rockbox-RCC

commit 01482b52047755567c1d1dd4b1ff87b09e0353ed

Blind-add some code from ipodlinux that hopefully fixes the colour problem on iPod Photo reported on the forums.
git-svn-id: svn://svn.rockbox.org/rockbox/trunk@17715 a1c6a512-1295-4272-9138-f99709370657

firmware/target/arm/ipod/lcd-color_nano.c
@@ -102,6 +102,17 @@ void lcd_init_device(void)
             lcd_type = 1;
         }
     }
+	if (lcd_type == 0) {
+		lcd_cmd_data(0xef, 0x0);
+		lcd_cmd_data(0x1, 0x0);
+		lcd_cmd_data(0x80, 0x1);
+		lcd_cmd_data(0x10, 0x8);
+		lcd_cmd_data(0x18, 0x6);
+		lcd_cmd_data(0x7e, 0x4);
+		lcd_cmd_data(0x7e, 0x5);
+		lcd_cmd_data(0x7f, 0x1);
+	}
+
 #elif CONFIG_LCD == LCD_IPODNANO
     /* iPodLinux doesn't appear have any LCD init code for the Nano */
 #endif

commit 376dfa7358bc0ef160e9ee9d8ff2ea9581347867

Fix the iPod Photo/Color "type 0 LCD" bug introduced in r17715 by correctly initialising the LCD to accept big-endian data (the same format as the "type 1" LCDs).  Also some tab policing.  Closes FS#9122 and FS#9097
git-svn-id: svn://svn.rockbox.org/rockbox/trunk@18079 a1c6a512-1295-4272-9138-f99709370657

firmware/target/arm/ipod/lcd-color_nano.c
@@ -104,16 +104,16 @@ void lcd_init_device(void)
             lcd_type = 1;
         }
     }
-	if (lcd_type == 0) {
-		lcd_cmd_data(0xef, 0x0);
-		lcd_cmd_data(0x1, 0x0);
-		lcd_cmd_data(0x80, 0x1);
-		lcd_cmd_data(0x10, 0x8);
-		lcd_cmd_data(0x18, 0x6);
-		lcd_cmd_data(0x7e, 0x4);
-		lcd_cmd_data(0x7e, 0x5);
-		lcd_cmd_data(0x7f, 0x1);
-	}
+    if (lcd_type == 0) {
+        lcd_cmd_data(0xef, 0x0);
+        lcd_cmd_data(0x1, 0x0);
+        lcd_cmd_data(0x80, 0x1);
+        lcd_cmd_data(0x10, 0xc);
+        lcd_cmd_data(0x18, 0x6);
+        lcd_cmd_data(0x7e, 0x4);
+        lcd_cmd_data(0x7e, 0x5);
+        lcd_cmd_data(0x7f, 0x1);
+    }
 
 #elif CONFIG_LCD == LCD_IPODNANO
     /* iPodLinux doesn't appear have any LCD init code for the Nano */