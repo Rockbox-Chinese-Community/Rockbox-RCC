Rockbox-Chinese-Community/Rockbox-RCC

commit bc17c10d43bab36265275c14f35d4f9e04821164

AS3525 SD : fix 2nd initialization (in loaded firmware)
git-svn-id: svn://svn.rockbox.org/rockbox/trunk@19049 a1c6a512-1295-4272-9138-f99709370657

firmware/target/arm/as3525/ata_sd_as3525.c
@@ -187,7 +187,6 @@ static int sd_init_card(const int drive)
 {
     unsigned int  c_size;
     unsigned long c_mult;
-
     int response;
     int max_tries = 100; /* max acmd41 attemps */
     bool sdhc;
@@ -372,11 +371,10 @@ static void init_pl180_controller(const int drive)
     MMC_CLOCK(drive) = MCI_CLOCK_ENABLE;
     MMC_CLOCK(drive) &= ~MCI_CLOCK_POWERSAVE;
 
+#else /* controller already initialized by bootloader */
+
     /* set MCLK divider */
     mci_set_clock_divider(drive, 200);
-#else
-    /* controller already initialized by bootloader */
-    (void)drive;
 #endif /* BOOTLOADER */
 }
 
@@ -384,6 +382,7 @@ int sd_init(void)
 {
     int ret;
 
+#ifdef BOOTLOADER /* No need to do twice the same thing */
     CGU_IDE =   (1<<7)  /* AHB interface enable */  |
                 (1<<6)  /* interface enable */      |
                 (2<<2)  /* clock didiver = 2+1 */   |
@@ -397,6 +396,7 @@ int sd_init(void)
     CCU_IO &= ~8;           /* bits 3:2 = 01, xpd is SD interface */
     CCU_IO |= 4;
 
+#endif
     init_pl180_controller(NAND_AS3525);
     ret = sd_init_card(NAND_AS3525);
     if(ret < 0)

commit 14e5b084cb7c7ac9eb676084a1e351c4b329845c

Fix as3525 bootloader broken by r19049 - we need to operate between 100kHz and 400kHz for the identification phase
git-svn-id: svn://svn.rockbox.org/rockbox/trunk@19056 a1c6a512-1295-4272-9138-f99709370657

firmware/target/arm/as3525/ata_sd_as3525.c
@@ -370,12 +370,10 @@ static void init_pl180_controller(const int drive)
 
     MMC_CLOCK(drive) = MCI_CLOCK_ENABLE;
     MMC_CLOCK(drive) &= ~MCI_CLOCK_POWERSAVE;
-
-#else /* controller already initialized by bootloader */
+#endif /* BOOTLOADER */
 
     /* set MCLK divider */
     mci_set_clock_divider(drive, 200);
-#endif /* BOOTLOADER */
 }
 
 int sd_init(void)