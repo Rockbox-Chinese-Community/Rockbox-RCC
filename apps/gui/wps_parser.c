Rockbox-Chinese-Community/Rockbox-RCC

commit 4e8efab0f6eb34cdd60a5d0aa1901a39dbb4405b

* Make %t stricter by aborting if a value is not given. * Move the default value for %mv back into wps_parser.c where it belongs * Add the debug line for the %mv tag
git-svn-id: svn://svn.rockbox.org/rockbox/trunk@18773 a1c6a512-1295-4272-9138-f99709370657

apps/gui/wps_parser.c
@@ -802,6 +802,18 @@ static int parse_timeout(const char *wps_bufptr,
     if (have_tenth == false)
         val *= 10;
 
+    if (val == 0 && skip == 0)
+    {
+        /* decide what to do if no value was specified */
+        switch (token->type)
+        {
+            case WPS_TOKEN_SUBLINE_TIMEOUT:
+                return -1;
+            case WPS_TOKEN_BUTTON_VOLUME:
+                val = HZ;
+                break;
+        }
+    }
     token->value.i = val;
 
     return skip;

apps/gui/gwps-common.c
@@ -1389,8 +1389,6 @@ static const char *get_token_value(struct gui_wps *gwps,
                 return NULL;
 #endif
         case WPS_TOKEN_BUTTON_VOLUME:
-            if (token->value.i == 0)
-                token->value.i = HZ;
             if (data->button_time_volume && 
                 TIME_BEFORE(current_tick, data->button_time_volume +
                                           token->value.i))

apps/gui/wps_debug.c
@@ -431,7 +431,10 @@ static char *get_token_desc(struct wps_token *token, struct wps_data *data,
             snprintf(buf, bufsize, "enable VP:%d",
                      token->value.i);
             break;
-
+        case WPS_TOKEN_BUTTON_VOLUME:
+            snprintf(buf, bufsize, "Volume button timeout:%d",
+                     token->value.i);
+            break;
         default:
             snprintf(buf, bufsize, "FIXME (code: %d)",
                     token->type);

commit 20c9c4d48783d06b03f791a43683fdbc0cd0d7f0

Add the required #include for the HZ define - fixes compilation of checkwps broken by r18773
git-svn-id: svn://svn.rockbox.org/rockbox/trunk@19113 a1c6a512-1295-4272-9138-f99709370657

apps/gui/wps_parser.c
@@ -26,6 +26,7 @@
 #include "file.h"
 #include "misc.h"
 #include "plugin.h"
+#include "kernel.h"
 
 #ifdef __PCTOOL__
 #ifdef WPSEDITOR