Rockbox-Chinese-Community/Rockbox-RCC

commit 85d3573aa26ee91d4365727814a53c1845e215c9

calendar: fix bug when add new event text longer than last entry. blackjack: fix bug that bet becomes 10 when resume saved game. bubbles: save high level to sparate file. clix, spacerocks, jewels, bubbles: correct text of menu item. wormlet: clean up code: removed unused defines and functions. pluginlib display_text: insert sleep so that the screen doesn't quit immediately.
git-svn-id: svn://svn.rockbox.org/rockbox/trunk@22143 a1c6a512-1295-4272-9138-f99709370657

apps/plugins/calendar.c
@@ -272,7 +272,7 @@ static int is_leap_year(int yr)
     (!((yr) % 4) && ((yr) % 100)) || !((yr) % 400))  ? 1:0 ;
 }
 
-/* searches the weekday of the first day in month, 
+/* searches the weekday of the first day in month,
  * relative to the given values */
 static int calc_weekday( struct shown *shown )
 {
@@ -343,19 +343,9 @@ static void draw_calendar(struct shown *shown)
     int x,y,pos,days_per_month,j;
     char buffer[9];
     const char *monthname[] = {
-                          "Jan",
-                          "Feb",
-                          "Mar",
-                          "Apr",
-                          "May",
-                          "Jun",
-                          "Jul",
-                          "Aug",
-                          "Sep",
-                          "Oct",
-                          "Nov",
-                          "Dec"
-                        };
+        "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+        "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+    };
     if(use_system_font)
         rb->lcd_setfont(FONT_SYSFIXED);
     rb->lcd_getstringsize("A",&w,&h);
@@ -421,7 +411,7 @@ static int memos_in_shown_memory = 0;
 
 static void load_memo(struct shown *shown)
 {
-    int i, k, fp;
+    int k, fp;
     bool exit = false;
     char temp_memo1[2];
     char temp_memo2[3];
@@ -429,18 +419,6 @@ static void load_memo(struct shown *shown)
     temp_memo1[1] = 0;
     temp_memo2[2] = 0;
     temp_memo4[4] = 0;
-    for (k = 0; k < memos_in_memory; k++)
-    {
-        memos[k].day = 0;
-        memos[k].month = 0;
-        memos[k].file_pointer_start = 0;
-        memos[k].file_pointer_end = 0;
-        memos[k].year = 0;
-        memos[k].type = 0;
-        memos[k].wday = 0;
-        for (i = 0; i < MAX_CHAR_MEMO_LEN; i++)
-            memos[k].message[i] = 0;
-    }
     for (k = 1; k < 32; k++)
         day_has_memo[k] = false;
     for (k = 0; k < 7; k++)
@@ -449,12 +427,13 @@ static void load_memo(struct shown *shown)
     fp = rb->open(ROCKBOX_DIR "/.memo",O_RDONLY);
     if (fp > -1)
     {
-        int count = rb->filesize(fp);
         rb->lseek(fp, 0, SEEK_SET);
         while (!exit)
         {
-            memos[memos_in_memory].file_pointer_start = rb->lseek(fp, 0,
-                                                                  SEEK_CUR);
+            bool load_to_memory;
+            rb->memset(&memos[memos_in_memory].message, 0, MAX_CHAR_MEMO_LEN);
+            memos[memos_in_memory].file_pointer_start =
+                                                    rb->lseek(fp, 0, SEEK_CUR);
             if (rb->read(fp, temp_memo2, 2) == 2)
                 memos[memos_in_memory].day = rb->atoi(&temp_memo2[0]);
             else
@@ -472,7 +451,7 @@ static void load_memo(struct shown *shown)
             if (memos[memos_in_memory].year > (shown->year * 10))
                 memos[memos_in_memory].year = (memos[memos_in_memory].year -
                                                memos[memos_in_memory].month) /
-                    100;
+                                               100;
             if (rb->read(fp, temp_memo1, 1) == 1)
                 memos[memos_in_memory].wday = rb->atoi(&temp_memo1[0]);
             else
@@ -481,48 +460,16 @@ static void load_memo(struct shown *shown)
                 memos[memos_in_memory].type = rb->atoi(&temp_memo1[0]);
             else
                 memos[memos_in_memory].type = 0;
-            for (k = 0; k <= count; k++)
+            load_to_memory = ((memos[memos_in_memory].type < 2) ||
+                              ((memos[memos_in_memory].type == 2) &&
+                                  (memos[memos_in_memory].month == shown->mon)) ||
+                              ((memos[memos_in_memory].type > 2) &&
+                                  (memos[memos_in_memory].month == shown->mon) &&
+                                  (memos[memos_in_memory].year == shown->year)));
+            k = 0;
+            while (1)
             {
-                if (rb->read(fp, temp_memo1, 1) == 1)
-                {
-                    if (
-                        (memos[memos_in_memory].type < 2)
-                        ||
-                        (
-                            (memos[memos_in_memory].type == 2)
-                            &&
-                            (memos[memos_in_memory].month == shown->mon)
-                        )
-                        ||
-                        (
-                            (memos[memos_in_memory].type > 2)
-                            &&
-                            (memos[memos_in_memory].month == shown->mon)
-                            &&
-                            (memos[memos_in_memory].year == shown->year)
-                        )
-                       )
-                    {
-                        if (temp_memo1[0] == '\n')
-                        {
-                            if (memos[memos_in_memory].type > 0)
-                                day_has_memo[memos[memos_in_memory].day] =
-                                    true;
-                            else
-                                wday_has_memo[memos[memos_in_memory].wday] =
-                                    true;
-                            memos[memos_in_memory++].file_pointer_end =
-                                rb->lseek(fp, 0, SEEK_CUR);
-                        }
-                        else if ( (temp_memo1[0] != '\r') &&
-                                  (temp_memo1[0] != '\t') &&
-                                  k < MAX_CHAR_MEMO_LEN-1 )
-                            memos[memos_in_memory].message[k] = temp_memo1[0];
-                    }
-                    if (temp_memo1[0] == '\n')
-                        break;
-                }
-                else
+                if (rb->read(fp, temp_memo1, 1) != 1)
                 {
                     memos[memos_in_memory].day = 0;
                     memos[memos_in_memory].month = 0;
@@ -535,6 +482,26 @@ static void load_memo(struct shown *shown)
                     exit = true;
                     break;
                 }
+                if (load_to_memory)
+                {
+                    if (temp_memo1[0] == '\n')
+                    {
+                        if (memos[memos_in_memory].type > 0)
+                            day_has_memo[memos[memos_in_memory].day] =
+                                true;
+                        else
+                            wday_has_memo[memos[memos_in_memory].wday] =
+                                true;
+                        memos[memos_in_memory++].file_pointer_end =
+                            rb->lseek(fp, 0, SEEK_CUR);
+                    }
+                    else if ( (temp_memo1[0] != '\r') &&
+                              (temp_memo1[0] != '\t') &&
+                              k < MAX_CHAR_MEMO_LEN-1 )
+                        memos[memos_in_memory].message[k++] = temp_memo1[0];
+                }
+                if (temp_memo1[0] == '\n')
+                    break;
             }
         }
     }
@@ -546,7 +513,7 @@ static bool save_memo(int changed, bool new_mod, struct shown *shown)
     int fp,fq;
     fp = rb->open(ROCKBOX_DIR "/.memo",O_RDONLY | O_CREAT);
     fq = rb->creat(ROCKBOX_DIR "/~temp");
-    if ( (fq != -1) && (fp != -1) )
+    if ( (fq > -1) && (fp > -1) )
     {
         int i;
         char temp[MAX_CHAR_MEMO_LEN];
@@ -578,9 +545,9 @@ static bool save_memo(int changed, bool new_mod, struct shown *shown)
         load_memo(shown);
         return true;
     }
-    else if (fp != -1)
+    else if (fp > -1)
         rb->close(fp);
-    else if (fq != -1)
+    else if (fq > -1)
         rb->close(fq);
     return false;
 }
@@ -714,7 +681,7 @@ static bool view_events(int selected, struct shown *shown)
     rb->gui_synclist_set_nb_items(&gui_memos, memos_in_shown_memory);
     rb->gui_synclist_select_item(&gui_memos, selected);
     rb->gui_synclist_draw(&gui_memos);
-     
+
     while (!exit)
     {
         button = rb->get_action(CONTEXT_LIST,TIMEOUT_BLOCK);
@@ -746,19 +713,10 @@ static void update_memos_shown(struct shown *shown)
     int i;
     memos_in_shown_memory = 0;
     for (i = 0; i < memos_in_memory; i++)
-        if (
-            (
-             (memos[i].type >= 1)
-             &&
-             (memos[i].day == shown->mday)
-            )
-            ||
-            (
-             (memos[i].type < 1)
-             &&
-             (memos[i].wday == shown->wday)
-            )
-           )
+        if (((memos[i].type >= 1) &&
+                (memos[i].day == shown->mday)) ||
+            ((memos[i].type < 1) &&
+                (memos[i].wday == shown->wday)))
             pointer_array[memos_in_shown_memory++] = i;
 }
 
@@ -836,7 +794,7 @@ enum plugin_status plugin_start(const void* parameter)
     struct shown shown;
     bool exit = false;
     int button;
-    
+
     (void)(parameter);
 
     calendar_init(&today, &shown);

apps/plugins/bubbles.c
@@ -36,12 +36,13 @@ PLUGIN_HEADER
 /* files */
 #define SCORE_FILE PLUGIN_GAMES_DIR "/bubbles.score"
 #define SAVE_FILE  PLUGIN_GAMES_DIR "/bubbles.save"
+#define DATA_FILE  PLUGIN_GAMES_DIR "/bubbles.data"
 
 /* final game return status */
 enum {
     BB_LOSE,
     BB_QUIT,
-    BB_QUIT_AND_SAVE,
+    BB_SAVE_AND_QUIT,
     BB_USB,
     BB_END,
     BB_WIN,
@@ -1241,7 +1242,6 @@ struct tile {
 /* the game context struct
  * score is the current score
  * level is the current level
- * highlevel is the highest level beaten
  * angle is the current cannon direction
  * shots is the number of shots fired since last compression
  * compress is the height of the compressor
@@ -1252,13 +1252,11 @@ struct tile {
  * elapsedlvl is level elapsed time in 1/100s of seconds
  * elapsedshot is the shot elapsed time in 1/100s of seconds
  * startedshot is when the current shot began
- * resume denotes whether to resume the currently loaded game
  * playboard is the game playing board
  */
 struct game_context {
     unsigned int score;
     unsigned int level;
-    unsigned int highlevel;
     int angle;
     int shots;
     int compress;
@@ -1276,6 +1274,8 @@ struct highscore highscores[NUM_SCORES];
 
 /* used to denote available resume info */
 static bool resume = false;
+static unsigned int highlevel = 0; /* the highest level beaten */
+static unsigned int last_highlevel = 0;
 
 static void bubbles_init(struct game_context* bb);
 static bool bubbles_nextlevel(struct game_context* bb);
@@ -1291,7 +1291,6 @@ static void bubbles_anchored(struct game_context* bb, int row, int col);
 static int  bubbles_fall(struct game_context* bb);
 static int  bubbles_checklevel(struct game_context* bb);
 static void bubbles_recordscore(struct game_context* bb);
-static void bubbles_savescores(struct game_context* bb);
 static bool bubbles_loadgame(struct game_context* bb);
 static void bubbles_savegame(struct game_context* bb);
 static inline void bubbles_setcolors(void);
@@ -1326,15 +1325,14 @@ static void bubbles_init(struct game_context* bb) {
 static bool bubbles_nextlevel(struct game_context* bb) {
     int i, j, pos;
 
-    /* save highest level */
-    if (bb->level > bb->highlevel)
-        bb->highlevel = bb->level;
-
     bb->level++;
 
     /* check if there are no more levels */
     if(bb->level > NUM_LEVELS) return false;
 
+    /* save highest level */
+    if (bb->level-1 > highlevel)
+        highlevel = bb->level-1;
 
     /* set up the play board */
     rb->memset(bb->playboard, 0, sizeof(bb->playboard));
@@ -2165,7 +2163,7 @@ static void bubbles_recordscore(struct game_context* bb) {
 
     int position;
 
-    position = highscore_update(bb->score, bb->level, "",
+    position = highscore_update(bb->score, bb->level-1, "",
                                 highscores, NUM_SCORES);
     if (position==0)
         rb->splash(HZ*2, "New High Score");
@@ -2174,36 +2172,43 @@ static void bubbles_recordscore(struct game_context* bb) {
 }
 
 /*****************************************************************************
-* bubbles_loadscores() loads the high scores saved file.
+* bubbles_loaddata() loads highest level beaten.
 ******************************************************************************/
-static void bubbles_loadscores(struct game_context* bb) {
+static void bubbles_loaddata(void) {
+    int fd;
 
-    int i;
-    int highlevel = 0;
-    /* highlevel and highscores */
-    highscore_load(SCORE_FILE, highscores, NUM_SCORES);
+    last_highlevel = highlevel = 0;
+    /* open data file */
+    fd = rb->open(DATA_FILE, O_RDONLY);
+    if (fd < 0) return;
 
-    /* level X in the high scores means one succeeded the level before (X-1) */
-    for (i = 0; i < NUM_SCORES; i++)
+    /* read in saved game */
+    if (rb->read(fd, &highlevel, sizeof(highlevel)) < (long)sizeof(highlevel))
     {
-        if (highscores[i].level-1 > highlevel)
-        {
-            highlevel = highscores[i].level-1;
-        }
+        highlevel = 0;
     }
+    if (highlevel >= NUM_LEVELS)
+        highlevel = NUM_LEVELS-1;
+    last_highlevel = highlevel;
 
-    if (bb->highlevel < (unsigned)highlevel)
-        bb->highlevel = highlevel;
+    rb->close(fd);
 }
 
 /*****************************************************************************
-* bubbles_savescores() saves the high scores saved file.
+* bubbles_savedata() saves the current game state.
 ******************************************************************************/
-static void bubbles_savescores(struct game_context* bb) {
+static void bubbles_savedata(void) {
+    int fd;
 
-    /* highscores */
-    (void)bb;
-    highscore_save(SCORE_FILE, highscores, NUM_SCORES+1);
+    if (last_highlevel >= highlevel) /* no need to save */
+        return;
+
+    fd = rb->open(DATA_FILE, O_WRONLY|O_CREAT);
+    if (fd < 0) return;
+
+    rb->write(fd, &highlevel, sizeof(highlevel));
+
+    rb->close(fd);
 }
 
 /*****************************************************************************
@@ -2225,6 +2230,9 @@ static bool bubbles_loadgame(struct game_context* bb) {
     }
 
     rb->close(fd);
+
+    /* delete saved file */
+    rb->remove(SAVE_FILE);
     return ret;
 }
 
@@ -2266,8 +2274,8 @@ static inline void bubbles_setcolors(void) {
 *     on usb connect and shutdown.
 ******************************************************************************/
 static void bubbles_callback(void* param) {
-    struct game_context* bb = (struct game_context*) param;
-    bubbles_savescores(bb);
+    (void) param;
+    highscore_save(SCORE_FILE, highscores, NUM_SCORES);
 }
 
 /*****************************************************************************
@@ -2353,23 +2361,20 @@ static int bubbles_handlebuttons(struct game_context* bb, bool animblock,
 }
 
 /*****************************************************************************
-* bubbles() is the main game subroutine, it returns the final game status.
+* bubbles_menu() is the initial menu at the start of the game.
 ******************************************************************************/
-static int bubbles(struct game_context* bb) {
-    int buttonres;
-    unsigned int startlevel = 0;
+static int bubbles_menu(struct game_context* bb) {
+    static unsigned int startlevel = 0;
+    int selected = resume?0:1;
     bool startgame = false;
-    long timeout;
 
-    /********************
-    *       menu        *
-    ********************/
     MENUITEM_STRINGLIST(menu,"Bubbles Menu",NULL,
-                         "Resume Game", "Start New Game",
+                        "Resume Game", "Start New Game",
                         "Level", "High Scores", "Playback Control",
-                        "Quit", "Quit and Save");
+                        "Quit", "Save and Quit");
+
     while(!startgame){
-        switch (rb->do_menu(&menu, NULL, NULL, false))
+        switch (rb->do_menu(&menu, &selected, NULL, false))
         {
             case 0: /* resume game */
                 if (!resume)
@@ -2379,11 +2384,8 @@ static int bubbles(struct game_context* bb) {
                 }
                 else
                 {
-                    startlevel = bb->level - 1;
                     startgame = true;
                 }
-                /* delete saved file */
-                rb->remove(SAVE_FILE);
                 break;
             case 1: /* new game */
                 bb->level = startlevel;
@@ -2393,7 +2395,7 @@ static int bubbles(struct game_context* bb) {
             case 2: /* choose level */
                 startlevel++;
                 rb->set_int("Choose start level", "", UNIT_INT, &startlevel,
-                            NULL, 1, 1, MIN(NUM_LEVELS,bb->highlevel+1), NULL);
+                            NULL, 1, 1, highlevel+1, NULL);
                 startlevel--;
                 break;
             case 3: /* High scores */
@@ -2404,13 +2406,30 @@ static int bubbles(struct game_context* bb) {
                 break;
             case 5: /* quit */
                 return BB_QUIT;
-            case 6: /* quit and save */
-                return BB_QUIT_AND_SAVE;
+            case 6: /* save and quit  */
+                return BB_SAVE_AND_QUIT;
             case MENU_ATTACHED_USB:
                 bubbles_callback(bb);
                 return BB_USB;
         }
     }
+    return 0;
+}
+
+/*****************************************************************************
+* bubbles() is the main game subroutine, it returns the final game status.
+******************************************************************************/
+static int bubbles(struct game_context* bb) {
+    int buttonres;
+    long timeout;
+
+    /********************
+    *       menu        *
+    ********************/
+    buttonres = bubbles_menu(bb);
+    if(buttonres != 0)
+        return buttonres;
+
     /********************
     *       init        *
     ********************/
@@ -2464,13 +2483,12 @@ enum plugin_status plugin_start(const void* parameter) {
     bool exit = false;
     enum plugin_status ret = PLUGIN_OK;
 
-    /* plugin init */
     (void)parameter;
-    /* end of plugin init */
 
     /* load files */
     resume = bubbles_loadgame(&bb);
-    bubbles_loadscores(&bb);
+    bubbles_loaddata();
+    highscore_load(SCORE_FILE, highscores, NUM_SCORES);
     rb->lcd_clear_display();
 
     /* start app */
@@ -2484,7 +2502,7 @@ enum plugin_status plugin_start(const void* parameter) {
             case BB_WIN:
                 rb->splash(HZ*2, "You Win!");
                 /* record high level */
-                bb.highlevel = NUM_LEVELS;
+                highlevel = NUM_LEVELS-1;
                 /* record high score */
                 bubbles_recordscore(&bb);
                 break;
@@ -2501,15 +2519,17 @@ enum plugin_status plugin_start(const void* parameter) {
 
             case BB_USB:
                 ret = PLUGIN_USB_CONNECTED;
+                exit = true;
                 break;
 
-            case BB_QUIT_AND_SAVE:
-                rb->splash(HZ/2, "Saving Game and Scors");
-                bubbles_savescores(&bb);
+            case BB_SAVE_AND_QUIT:
+                rb->splash(HZ/2, "Saving Game ...");
                 bubbles_savegame(&bb);
                 /* fall through */
 
             case BB_QUIT:
+                bubbles_savedata();
+                highscore_save(SCORE_FILE, highscores, NUM_SCORES);
                 exit = true;
                 break;
 

apps/plugins/clix.c
@@ -605,7 +605,7 @@ static int clix_menu(struct clix_game_state_t* state, bool ingame)
                              "Resume Game",
                              "Start New Game",
                              "Help",
-                             "High Score",
+                             "High Scores",
                              "Playback Control",
                              "Quit");
 

apps/plugins/jewels.c
@@ -1323,7 +1323,7 @@ static int jewels_game_menu(struct game_context* bj, bool ingame)
                              "Start New Game",
                              "Mode",
                              "Help",
-                             "High Score",
+                             "High Scores",
                              "Playback Control",
                              "Quit without Saving",
                              "Quit");

apps/plugins/lib/highscore.c
@@ -129,7 +129,6 @@ void highscore_show(int position, struct highscore *scores, int num_scores, bool
     rb->lcd_set_background(LCD_BLACK);
     rb->lcd_set_foreground(LCD_WHITE);
 #endif
-    rb->button_clear_queue();
     rb->lcd_clear_display();
 
     rb->lcd_setfont(FONT_UI);
@@ -174,6 +173,8 @@ void highscore_show(int position, struct highscore *scores, int num_scores, bool
         }
     }
     rb->lcd_update();
+    rb->sleep(HZ/2);
+    rb->button_clear_queue();
     rb->button_get(true);
     rb->lcd_setfont(FONT_SYSFIXED);
 }

apps/plugins/rocklife.c
@@ -77,7 +77,7 @@ PLUGIN_HEADER
 #define PATTERN_GROWTH_1   1
 #define PATTERN_GROWTH_2   2
 #define PATTERN_ACORN      3
-#define PATTERN_GLIDER_GUN 4 /* not yet implemented */
+#define PATTERN_GLIDER_GUN 4
 
 const struct button_mapping *plugin_contexts[]
 = {generic_directions, generic_actions};

apps/plugins/spacerocks.c
@@ -644,7 +644,7 @@ static int spacerocks_menu(bool ingame)
                              "Resume Game",
                              "Start New Game",
                              "Help",
-                             "High Score",
+                             "High Scores",
                              "Playback Control",
                              "Quit");
                              

commit 8e1094766150795d4ce261234ab67c9caebdcde1

fix bug calendar fails to add new memo if there is not .memo file caused by my change in r22143. setting flags O_RDONLY|O_CREAT for open doesn't seem to create file. change flags to O_RDWR|O_CREAT but there is no writing.
git-svn-id: svn://svn.rockbox.org/rockbox/trunk@22169 a1c6a512-1295-4272-9138-f99709370657

apps/plugins/calendar.c
@@ -514,14 +514,15 @@ static void load_memo(struct shown *shown)
                     break;
             }
         }
+        rb->close(fp);
     }
-    rb->close(fp);
 }
 
 static bool save_memo(int changed, bool new_mod, struct shown *shown)
 {
     int fp,fq;
-    fp = rb->open(ROCKBOX_DIR "/.memo",O_RDONLY | O_CREAT);
+    /* use O_RDWR|O_CREAT so that file is created if it doesn't exist. */
+    fp = rb->open(ROCKBOX_DIR "/.memo", O_RDWR|O_CREAT);
     fq = rb->creat(ROCKBOX_DIR "/~temp");
     if ( (fq > -1) && (fp > -1) )
     {