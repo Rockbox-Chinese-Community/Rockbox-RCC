Rockbox-Chinese-Community/Rockbox-RCC

commit 6e7aca2d5092c2dbf7d02dd7b4bcff062a926fb0

Change a few defines to enums to match the other code around and argument bits #defines for settings_load to make clear it's bitwise.
git-svn-id: svn://svn.rockbox.org/rockbox/trunk@22192 a1c6a512-1295-4272-9138-f99709370657

apps/settings.h
@@ -83,12 +83,14 @@ struct opt_items {
 #define MAX_FILENAME 32
 
 
-#define BOOKMARK_NO  0
-#define BOOKMARK_YES 1
-#define BOOKMARK_ASK 2
-#define BOOKMARK_UNIQUE_ONLY 2
-#define BOOKMARK_RECENT_ONLY_YES 3
-#define BOOKMARK_RECENT_ONLY_ASK 4
+enum {
+    BOOKMARK_NO,
+    BOOKMARK_YES,
+    BOOKMARK_ASK,
+    BOOKMARK_UNIQUE_ONLY,
+    BOOKMARK_RECENT_ONLY_YES,
+    BOOKMARK_RECENT_ONLY_ASK,
+};
 
 enum
 {
@@ -104,14 +106,18 @@ enum
     TRIG_TYPE_NEW_FILE
 };
 
-#define CROSSFADE_ENABLE_SHUFFLE                1
-#define CROSSFADE_ENABLE_TRACKSKIP              2
-#define CROSSFADE_ENABLE_SHUFFLE_AND_TRACKSKIP  3
-#define CROSSFADE_ENABLE_ALWAYS                 4
+enum {
+    CROSSFADE_ENABLE_SHUFFLE,
+    CROSSFADE_ENABLE_TRACKSKIP,
+    CROSSFADE_ENABLE_SHUFFLE_AND_TRACKSKIP,
+    CROSSFADE_ENABLE_ALWAYS,
+};
 
-#define FOLDER_ADVANCE_OFF 0
-#define FOLDER_ADVANCE_NEXT 1
-#define FOLDER_ADVANCE_RANDOM 2
+enum {
+    FOLDER_ADVANCE_OFF,
+    FOLDER_ADVANCE_NEXT,
+    FOLDER_ADVANCE_RANDOM,
+}
 
 /* repeat mode options */
 enum
@@ -210,9 +216,9 @@ enum {  ALARM_START_WPS = 0,
 /** function prototypes **/
 
 /* argument bits for settings_load() */
-#define SETTINGS_RTC 1 /* only the settings from the RTC nonvolatile RAM */
-#define SETTINGS_HD  2 /* only the settings from the disk sector */
-#define SETTINGS_ALL 3 /* both */
+#define SETTINGS_RTC (BIT_N(0)) /* only the settings from the RTC nonvolatile RAM */
+#define SETTINGS_HD  (BIT_N(1)) /* only the settings from the disk sector */
+#define SETTINGS_ALL (SETTINGS_RTC|SETTINGS_HD) /* both */
 void settings_load(int which);
 bool settings_load_config(const char* file, bool apply);
 

commit 3e2cfeb78a30701dfbaabfc7b5de46e2b01e9e72

fix crossfade setting broken by r22192.
git-svn-id: svn://svn.rockbox.org/rockbox/trunk@22343 a1c6a512-1295-4272-9138-f99709370657

apps/settings.h
@@ -107,6 +107,7 @@ enum
 };
 
 enum {
+    CROSSFADE_ENABLE_OFF = 0,
     CROSSFADE_ENABLE_SHUFFLE,
     CROSSFADE_ENABLE_TRACKSKIP,
     CROSSFADE_ENABLE_SHUFFLE_AND_TRACKSKIP,
@@ -114,7 +115,7 @@ enum {
 };
 
 enum {
-    FOLDER_ADVANCE_OFF,
+    FOLDER_ADVANCE_OFF = 0,
     FOLDER_ADVANCE_NEXT,
     FOLDER_ADVANCE_RANDOM,
 };
@@ -122,7 +123,7 @@ enum {
 /* repeat mode options */
 enum
 {
-    REPEAT_OFF,
+    REPEAT_OFF = 0,
     REPEAT_ALL,
     REPEAT_ONE,
     REPEAT_SHUFFLE,