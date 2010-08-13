Rockbox-Chinese-Community/Rockbox-RCC

commit 71421cd678f896a7073fd72178c184d0a2dde5fd

Backdrop dir needs to go through get_user_file_path()
git-svn-id: svn://svn.rockbox.org/rockbox/trunk@27791 a1c6a512-1295-4272-9138-f99709370657

apps/gui/skin_engine/skin_backdrops.c
@@ -52,6 +52,8 @@ char* skin_backdrop_load(char* backdrop, char *bmpdir, enum screen_type screen)
 {
     int i;
     struct skin_backdrop *bdrop = NULL;
+    char dir[MAX_PATH];
+    const char *backdrop_dir = get_user_file_path(bmpdir, 0, dir, sizeof(dir));
     char filename[MAX_PATH];
     size_t buf_size;
     bool loaded = false;
@@ -78,12 +80,12 @@ char* skin_backdrop_load(char* backdrop, char *bmpdir, enum screen_type screen)
                 return NULL; /* backdrop setting not set */
             }
             snprintf(filename, sizeof(filename), "%s/%s.bmp",
-                     BACKDROP_DIR, global_settings.backdrop_file);
+                     backdrop_dir, global_settings.backdrop_file);
         }
     }
     else
     {
-        get_image_filename(backdrop, bmpdir, filename, sizeof(filename));
+        get_image_filename(backdrop, backdrop_dir, filename, sizeof(filename));
     }
     
     for(i=0;i<SKINNABLE_SCREENS_COUNT*NB_SCREENS;i++)

commit 18c29646fe791e4f369cb92188283a5be672f1a7

Fix backdrops from sbs not being loaded correctly (caused by r27791).
git-svn-id: svn://svn.rockbox.org/rockbox/trunk@27802 a1c6a512-1295-4272-9138-f99709370657

apps/gui/skin_engine/skin_backdrops.c
@@ -53,7 +53,6 @@ char* skin_backdrop_load(char* backdrop, char *bmpdir, enum screen_type screen)
     int i;
     struct skin_backdrop *bdrop = NULL;
     char dir[MAX_PATH];
-    const char *backdrop_dir = get_user_file_path(bmpdir, 0, dir, sizeof(dir));
     char filename[MAX_PATH];
     size_t buf_size;
     bool loaded = false;
@@ -80,12 +79,14 @@ char* skin_backdrop_load(char* backdrop, char *bmpdir, enum screen_type screen)
                 return NULL; /* backdrop setting not set */
             }
             snprintf(filename, sizeof(filename), "%s/%s.bmp",
-                     backdrop_dir, global_settings.backdrop_file);
+                     get_user_file_path(BACKDROP_DIR, 0, dir, sizeof(dir)),
+                     global_settings.backdrop_file);
         }
     }
     else
     {
-        get_image_filename(backdrop, backdrop_dir, filename, sizeof(filename));
+        const char *bd_dir = get_user_file_path(bmpdir, 0, dir, sizeof(dir));
+        get_image_filename(backdrop, bd_dir, filename, sizeof(filename));
     }
     
     for(i=0;i<SKINNABLE_SCREENS_COUNT*NB_SCREENS;i++)