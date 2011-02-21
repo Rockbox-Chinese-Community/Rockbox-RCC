Rockbox-Chinese-Community/Rockbox-RCC

commit 1d43096f89400f82ef31b5b7804351f470a6c548

FS#11920: Do not overwrite already existing metadata and take into account string termination. This can save several bytes of the metadata buffer when tags have multiple entries (e.g. multiple gerne tags)
git-svn-id: svn://svn.rockbox.org/rockbox/trunk@29349 a1c6a512-1295-4272-9138-f99709370657

apps/metadata/id3tags.c
@@ -358,8 +358,7 @@ static int parseuser( struct mp3entry* entry, char* tag, int bufferpos )
 
     if ((tag - entry->id3v2buf + desc_len + 2) < bufferpos) {
         /* At least part of the value was read, so we can safely try to
-         * parse it
-         */
+         * parse it */
         value = tag + desc_len + 1;
         value_len = bufferpos - (tag - entry->id3v2buf);
         
@@ -368,8 +367,7 @@ static int parseuser( struct mp3entry* entry, char* tag, int bufferpos )
             entry->albumartist = tag;
 #if CONFIG_CODEC == SWCODEC
         } else {
-            value_len = parse_replaygain(tag, value, entry, tag,
-                value_len);
+            value_len = parse_replaygain(tag, value, entry, tag, value_len);
 #endif
         }
     }
@@ -1040,6 +1038,12 @@ void setid3v2title(int fd, struct mp3entry *entry)
 #endif
                 if( tr->ppFunc )
                     bufferpos = tr->ppFunc(entry, tag, bufferpos);
+                    
+                /* Trim. Take into account that multiple string contents will
+                 * only be displayed up to their first null termination. All
+                 * content after this null termination is obsolete and can be
+                 * overwritten. */
+                bufferpos -= (bytesread - strlen(tag));
 
                 /* Seek to the next frame */
                 if(framelen < totframelen)

apps/metadata/metadata_common.c
@@ -337,7 +337,10 @@ long parse_tag(const char* name, char* value, struct mp3entry* id3,
         p = NULL;
     }
     
-    if (p)
+    /* Do not overwrite already available metadata. Especially when reading
+     * tags with e.g. multiple genres / artists. This way only the first 
+     * of multiple entries is used, all following are dropped. */
+    if (p!=NULL && *p==NULL)
     {
         len = strlen(value);
         len = MIN(len, buf_remaining - 1);

apps/metadata/mp4.c
@@ -117,11 +117,17 @@ static unsigned int read_mp4_tag_string(int fd, int size_left, char** buffer,
 
     if (bytes_read)
     {
-        (*buffer)[bytes_read] = 0;
-        *dest = *buffer;
-        length = strlen(*buffer) + 1;
-        *buffer_left -= length;
-        *buffer += length;
+        /* Do not overwrite already available metadata. Especially when reading
+         * tags with e.g. multiple genres / artists. This way only the first 
+         * of multiple entries is used, all following are dropped. */
+        if (*dest == NULL)
+        {
+            (*buffer)[bytes_read] = 0;
+            *dest = *buffer;
+            length = strlen(*buffer) + 1;
+            *buffer_left -= length;
+            *buffer += length;
+        }
     }
     else
     {

apps/plugins/SOURCES
@@ -217,7 +217,7 @@ superdom.c
 
 
 
-#ifdef HAVE_TEST_PLUGINS /* enable in advanced build options */
+#if 1//def HAVE_TEST_PLUGINS /* enable in advanced build options */
 #ifdef HAVE_ADJUSTABLE_CPU_FREQ
 test_boost.c
 #endif

commit 48c60faf0628587270da0e472e4e1cb0198b64e9

Fix FS#11955. ID3 tag parser was broken since r29349. Trimming strings must be done in the helper functions, not outside.
git-svn-id: svn://svn.rockbox.org/rockbox/trunk@29372 a1c6a512-1295-4272-9138-f99709370657

apps/metadata/id3tags.c
@@ -266,6 +266,9 @@ static int parseyearnum( struct mp3entry* entry, char* tag, int bufferpos )
 /* parse numeric genre from string, version 2.2 and 2.3 */
 static int parsegenre( struct mp3entry* entry, char* tag, int bufferpos )
 {
+    /* Use bufferpos to hold current position in entry->id3v2buf. */
+    bufferpos = tag - entry->id3v2buf;
+
     if(entry->id3version >= ID3_VER_2_4) {
         /* In version 2.4 and up, there are no parentheses, and the genre frame
            is a list of strings, either numbers or text. */
@@ -273,19 +276,19 @@ static int parsegenre( struct mp3entry* entry, char* tag, int bufferpos )
         /* Is it a number? */
         if(isdigit(tag[0])) {
             entry->genre_string = id3_get_num_genre(atoi( tag ));
-            return tag - entry->id3v2buf;
+            return bufferpos;
         } else {
             entry->genre_string = tag;
-            return bufferpos;
+            return bufferpos + strlen(tag) + 1;
         }
     } else {
         if( tag[0] == '(' && tag[1] != '(' ) {
             entry->genre_string = id3_get_num_genre(atoi( tag + 1 ));
-            return tag - entry->id3v2buf;
+            return bufferpos;
         }
         else {
             entry->genre_string = tag;
-            return bufferpos;
+            return bufferpos + strlen(tag) + 1;
         }
     }
 }
@@ -360,7 +363,7 @@ static int parseuser( struct mp3entry* entry, char* tag, int bufferpos )
         /* At least part of the value was read, so we can safely try to
          * parse it */
         value = tag + desc_len + 1;
-        value_len = bufferpos - (tag - entry->id3v2buf);
+        value_len = strlen(value) + 1;
         
         if (!strcasecmp(tag, "ALBUM ARTIST")) {
             strlcpy(tag, value, value_len);
@@ -368,6 +371,8 @@ static int parseuser( struct mp3entry* entry, char* tag, int bufferpos )
 #if CONFIG_CODEC == SWCODEC
         } else {
             value_len = parse_replaygain(tag, value, entry, tag, value_len);
+#else
+            value_len = 0;
 #endif
         }
     }
@@ -1038,12 +1043,6 @@ void setid3v2title(int fd, struct mp3entry *entry)
 #endif
                 if( tr->ppFunc )
                     bufferpos = tr->ppFunc(entry, tag, bufferpos);
-                    
-                /* Trim. Take into account that multiple string contents will
-                 * only be displayed up to their first null termination. All
-                 * content after this null termination is obsolete and can be
-                 * overwritten. */
-                bufferpos -= (bytesread - strlen(tag));
 
                 /* Seek to the next frame */
                 if(framelen < totframelen)