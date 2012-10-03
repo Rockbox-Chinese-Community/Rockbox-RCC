31f2dfbfbcca1eb3acd2f2567a541b4354ab2a77
zhkailing <zhkailing@163.com>
2012-10-03 22:54:09 +0800

    重新优化环绕和抗锯齿代码，修复和均衡器输出混乱的问题。（初始版本）

--- lib/rbcodec/dsp/surround.c
+++ lib/rbcodec/dsp/surround.c
@@ -1,21 +1,24 @@
-#include "surround_aatube.h"
+#include "surround.h"
 #include "config.h"
 #include "fixedpoint.h"
 #include "fracmul.h"
 #include "settings.h"
 #include "dsp_proc_entry.h"
-/* dolby surround */
-#define DOLBY_SURROUND_MAX 2048
+
+#define DOLBY_SURROUND_MAX 3072
 static int32_t dolbyBuffer[DOLBY_SURROUND_MAX * 2];
 static int32_t dolbyBufferPointer IBSS_ATTR;
 
 static bool surround_enabled = false;
-static bool aatube_enabled = false;
 
-/* antialias-tube */
-static int32_t aatube_filter_state[2] IBSS_ATTR;
- 
-/* Dolby surround */
+static void dsp_surround_flush(void)
+{
+    if (!surround_enabled)
+        return; 
+    memset(dolbyBuffer, 0, sizeof(dolbyBuffer));
+    dolbyBufferPointer = 0;
+}
+
 void dsp_surround_enable(bool enable)
 {
     surround_enabled = enable;
@@ -49,8 +52,8 @@ static void dolby_surround_process(struct dsp_proc_entry *this,
 
                 *inputSamplesB *= 0;
     	        inputSamplesB ++;
-                backBuffer += 2;
-                dolbyBufferPointer += 2;
+                backBuffer += 1;
+                dolbyBufferPointer += 1;
             }
         }
         else
@@ -87,39 +90,7 @@ static void dolby_surround_process(struct dsp_proc_entry *this,
             memcpy(dolbyBuffer, backBuffer, dolbyBufferPointer * 4);
         }
     }
-(void)this;
-}
-
-/* antialias tube */
-void dsp_aatube_enable(bool enable)
-{
-    aatube_enabled = enable;
-    struct dsp_config *dsp = dsp_get_config(CODEC_IDX_AUDIO);
-    dsp_proc_enable(dsp, DSP_PROC_AATUBE, enable);
-    dsp_proc_activate(dsp, DSP_PROC_AATUBE, true);
-}
-
-static void antialias_tube_process(struct dsp_proc_entry *this,
-                               struct dsp_buffer **buf_p)
-{
-    int32_t *pSmpCH0, *pSmpCH1;
-    int32_t i;
-    struct dsp_buffer *buf = *buf_p;
-    int count = buf->remcount;
-    const int num_chan = buf->format.num_channels;
-    {
-        pSmpCH0 = buf->p32[0];
-        pSmpCH1 = buf->p32[1];
-        for (int ch = 0; ch < num_chan; ch++)
-        {
-            for (i = 0; i < count; i++)
-            {
-                aatube_filter_state[ch] = (aatube_filter_state[ch] + pSmpCH0[i]) >> 1;
-                pSmpCH0[i] = aatube_filter_state[ch];
-            }
-        }
-    }
-(void)this;
+    (void)this;
 }
 
 /* DSP message hook */
@@ -132,39 +103,14 @@ static intptr_t surround_configure(struct dsp_proc_entry *this,
     {
     case DSP_PROC_INIT:
         if (value != 0)
-            break; /* Already enabled */
+            break;
         this->process[0] = dolby_surround_process;
         dsp_proc_activate(dsp, DSP_PROC_SURROUND, true);
         break;
-    case DSP_RESET:
     case DSP_FLUSH:
+        dsp_surround_flush();
         break;
-    case DSP_PROC_CLOSE:
-        dolbyBufferPointer = 0;
-    }
-
-    return 1;
-    (void)dsp;
-}
-
-static intptr_t aatube_configure(struct dsp_proc_entry *this,
-                                     struct dsp_config *dsp,
-                                     unsigned int setting,
-                                     intptr_t value)
-{
-    switch (setting)
-    {
-    case DSP_PROC_INIT:
-        if (value != 0)
-            break; /* Already enabled */
-        this->process[0] = antialias_tube_process;
-        dsp_proc_activate(dsp, DSP_PROC_AATUBE, true);
-        break;
-    case DSP_FLUSH:
-        break;
-    case DSP_PROC_CLOSE:
-        aatube_filter_state[0] = 0;
-	aatube_filter_state[1] = 0;
+   case DSP_PROC_CLOSE:
         break;
     }
 
@@ -176,7 +122,4 @@ static intptr_t aatube_configure(struct dsp_proc_entry *this,
 DSP_PROC_DB_ENTRY(
     SURROUND,
     surround_configure);
-DSP_PROC_DB_ENTRY(
-    AATUBE,
-    aatube_configure);
 
--- lib/rbcodec/dsp/surround.h
+++ lib/rbcodec/dsp/surround.h
@@ -1,3 +1,2 @@
 #include <stdbool.h>
 void dsp_surround_enable(bool enable);
-void dsp_aatube_enable(bool enable);
